d3d8f321d7530f2e371e1e7f583e74035a9695ab
Ammar-Amin <ammaramin5253@gmail.com>
2024-05-14 17:11:31 +0530

    completed UI for signUp & signIn pages

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignIn() {
     return (
-        <div>
-            SignIn
+        <div className='p-3 max-w-lg mx-auto'>
+            <h1 className='my-7 text-3xl text-center font-semibold'>Sign In</h1>
+            <form className='flex flex-col gap-4'>
+                <input type="email" className='p-3 border rounded-lg' name='email' placeholder='Email' />
+                <input type="password" className='p-3 border rounded-lg' name='password' placeholder='Password' />
+                <button className='my-1 p-3 text-white bg-slate-700 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>sign in</button>
+            </form>
+            <div className='flex gap-3 mt-4'>
+                <p>Don't have an account?</p>
+                <Link to='/sign-up'>
+                    <span className='text-blue-600'>Sign Up</span>
+                </Link>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
     return (
-        <div>
-            SignUp
+        <div className='p-3 max-w-lg mx-auto'>
+            <h1 className='my-7 text-3xl text-center font-semibold'>Sign Up</h1>
+            <form className='flex flex-col gap-4'>
+                <input type="text" className='p-3 border rounded-lg' name='username' placeholder='Username' />
+                <input type="email" className='p-3 border rounded-lg' name='email' placeholder='Email' />
+                <input type="password" className='p-3 border rounded-lg' name='password' placeholder='Password' />
+                <button className='my-1 p-3 text-white bg-slate-700 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>sign up</button>
+            </form>
+            <div className='flex gap-3 mt-4'>
+                <p>Already have an account?</p>
+                <Link to='/sign-in'>
+                    <span className='text-blue-600'>Sign In</span>
+                </Link>
+            </div>
         </div>
     )
 }
