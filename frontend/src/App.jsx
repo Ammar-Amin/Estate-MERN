8a9c0cc207fdbf1c188c8ab4d05ac14aacf0faef
Ammar-Amin <ammaramin5253@gmail.com>
2024-05-20 16:03:44 +0530

    updated header & made profile page protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/Header'
+import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route element={<ProtectedRoute />} >
+          <Route path='/profile' element={<Profile />} />
+        </Route>
         <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 function Header() {
+    let { currentUser } = useSelector(state => state.user)
+
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -25,8 +28,22 @@ function Header() {
                     <Link to='/about' >
                         <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                     </Link>
-                    <Link to='/sign-in' >
-                        <li className='text-slate-700 hover:underline'>Sign in</li>
+                    <Link to='/profile' >
+                        {
+                            currentUser
+                                ? (
+                                    <img
+                                        className='w-7 h-7 rounded-full object-cover'
+                                        src={currentUser.avatar}
+                                        alt='profile'
+                                    />
+                                )
+                                : (
+                                    <li className='text-slate-700 hover:underline'>
+                                        Sign in
+                                    </li>
+                                )
+                        }
                     </Link>
                 </ul>
             </div>
