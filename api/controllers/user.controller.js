b927ce3d4bce9f39aba4cb56da160fc757a612c9
Ammar-Amin <ammaramin5253@gmail.com>
2024-05-22 17:53:41 +0530

    create update User & get all User api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,6 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+import User from "../models/user.model.js";
 export const test = (req, res) => {
     res.json({
         status: 200,
@@ -5,4 +8,38 @@ export const test = (req, res) => {
         message: 'Welcome my friend',
         sender: 'test@example.com',
     })
+}
+
+export const allUsers = async (req, res) => {
+    try {
+        let allUsers = await User.find();
+        res.status(200).json(allUsers)
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only update your own Password'))
+
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, { new: true })
+
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest)
+
+    } catch (error) {
+        next(error)
+    }
 }
