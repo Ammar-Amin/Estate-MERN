2af89c058f302a1486fb08166d10c799f619edd6
Ammar-Amin <ammaramin5253@gmail.com>
2024-05-29 11:09:41 +0530

    create update listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,3 +1,4 @@
+import { mongoose } from "mongoose";
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -24,6 +25,30 @@ export const deleteListing = async (req, res, next) => {
     try {
         await Listing.findByIdAndDelete(req.params.id)
         res.status(200).json("Listing deleted successfully")
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const updateListing = async (req, res, next) => {
+
+    if (!mongoose.isValidObjectId(req.params.id)) return next(errorHandler(401, "id not valid"))
+    let listing = await Listing.findById(req.params.id)
+
+    if (!listing) {
+        return next(errorHandler(404, "Listing not found!"))
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "You can only update your own listing"));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
     } catch (err) {
         next(err);
     }
