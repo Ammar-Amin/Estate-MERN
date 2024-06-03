b09fd02ff10cca80986d32552beb30538c07da01
Ammar-Amin <ammaramin5253@gmail.com>
2024-06-03 13:49:33 +0530

    add pre installation for deployment

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config()
 
 const app = express();
@@ -16,6 +17,8 @@ mongoose
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log(err));
 
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
     console.log('listening on port : 3000')
 })
@@ -23,6 +26,14 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    // res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+    // OR
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
